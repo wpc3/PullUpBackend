0626021cee6cacc3f09594dc979a80bf29d4c686
Will <will@Zipcoders-MacBook-Pro-29.local>
2024-08-27 13:57:39 -0400

    first commit

---
+++ pullUpbackend/src/main/java/pullUp/pullUpbackend/PullUpbackendApplication.java
@@ -0,0 +1,13 @@
+package pullUp.pullUpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullUpbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PullUpbackendApplication.class, args);
+	}
+
+}
---
+++ pullUpbackend/src/test/java/pullUp/pullUpbackend/PullUpbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pullUp.pullUpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullUpbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
