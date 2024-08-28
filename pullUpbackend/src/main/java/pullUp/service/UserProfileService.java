1ad13c5c4c92538b20963e8a60941cb4feadd520
Will <will@Zipcoders-MacBook-Pro-29.local>
2024-08-27 21:37:06 -0400

    working on user profile service

--- pullUpbackend/src/main/java/pullUp/service/UserProfileService.java
+++ pullUpbackend/src/main/java/pullUp/service/UserProfileService.java
@@ -1,7 +1,25 @@
 package pullUp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import pullUp.model.UserProfile;
+import pullUp.repository.UserProfileRepository;
 
 @Service
 public class UserProfileService {
+private UserProfileRepository repository;
+
+public UserProfileService(@Autowired UserProfileRepository repository){
+    this.repository = repository;
+}
+
+public UserProfile create(UserProfile userProfileToPersist){
+    return repository.save(userProfileToPersist);
+}
+
+
+
+
 }
