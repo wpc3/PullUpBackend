855595862b84c9df039af708d2b2f8b9c857532d
Will <will@Zipcoders-MacBook-Pro-29.local>
2024-08-28 07:58:22 -0400

    put packages in the right spot smh

--- pullUpbackend/src/main/java/pullUp/repository/UserProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package pullUp.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import pullUp.model.UserProfile;
-
-public interface UserProfileRepository extends CrudRepository<UserProfile,Long> {
-}
