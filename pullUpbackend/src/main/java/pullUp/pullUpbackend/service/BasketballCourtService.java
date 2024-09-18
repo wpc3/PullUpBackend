d0c17cfa3df13fdf61e8cdfc3626ac31fe1f413c
Will <will@Zipcoders-MacBook-Pro-29.local>
2024-09-18 17:18:50 -0400

    got list courts working

--- pullUpbackend/src/main/java/pullUp/pullUpbackend/service/BasketballCourtService.java
+++ pullUpbackend/src/main/java/pullUp/pullUpbackend/service/BasketballCourtService.java
@@ -1,4 +1,28 @@
 package pullUp.pullUpbackend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pullUp.pullUpbackend.model.BasketballCourt;
+import pullUp.pullUpbackend.model.UserProfile;
+import pullUp.pullUpbackend.repository.BasketballCourtsRepository;
+import pullUp.pullUpbackend.repository.UserProfileRepository;
+
+import java.util.List;
+@Service
 public class BasketballCourtService {
+    private BasketballCourtsRepository repository;
+    private UserProfileRepository userProfileRepository;
+    public BasketballCourtService(@Autowired BasketballCourtsRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<BasketballCourt> findAllBasketballCourts(){
+        return (List<BasketballCourt>) repository.findAll();
+    }
+
+    public List<BasketballCourt> findAllCourtsSavedByUser(String username){
+        return null;
+    }
+
+
 }
