1ea243019b960f037e3c1e49b9c32b367269bc16
will chapman <williamchapman53@gmail.com>
2025-09-17 09:49:06 -0400

    fixed relationship with message and profile

--- pullUpbackend/src/main/java/pullUp/pullUpbackend/service/MessageService.java
+++ pullUpbackend/src/main/java/pullUp/pullUpbackend/service/MessageService.java
@@ -1,7 +1,21 @@
 package pullUp.pullUpbackend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pullUp.pullUpbackend.model.Message;
+import pullUp.pullUpbackend.repository.MessageRepository;
 
 @Service
 public class MessageService {
+
+    private MessageRepository messageRepository;
+
+
+    public MessageService(@Autowired MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public Message createAMessage(){
+        return  null;
+    }
 }
